d9ba03ac7de552356a21a0ff2b6ef164d87bdb7d
Lior Brauer <liorbrauer@users.noreply.github.com>
2023-01-18 09:19:08 +0000

    fix?

--- frontend/src/components/Item/utils/ItemFetcher.js
+++ frontend/src/components/Item/utils/ItemFetcher.js
@@ -4,5 +4,6 @@ export async function getItemAndComments(id) {
   const item = await agent.Items.get(id);
   const comments = await agent.Comments.forItem(id);
 
+  console.log("no change");
   return [item, comments];
 }
